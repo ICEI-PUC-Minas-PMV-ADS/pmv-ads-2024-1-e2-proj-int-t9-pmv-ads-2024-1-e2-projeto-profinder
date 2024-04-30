6df5408ffc7206db9e5188b44c16ba97a8617279
lpereira22 <137580591+lpereira22@users.noreply.github.com>
2024-04-30 00:00:25 -0300

    css update

--- src/código-fonte/js/meu_perfil.js
+++ src/código-fonte/js/meu_perfil.js
@@ -1,11 +1,25 @@
+const senha1 = document.querySelector("#senha-perfil")
+const senha2 = document.querySelector("#senha2-perfil")
+
 $(document).ready(function(){
 $('#cep-perfil').mask('00000-000');
 $('#telefone-perfil').mask('(00) 0000-0000');
 $('#celular-perfil').mask('(00) 00000-0000');
 });
 
-const senha = document.querySelector("#senha-perfil")
-senha.addEventListener('keypress' , () =>{
-    if(senha.value.length > 20)
-        senha.style.border = "2px solid red";
+
+senha1.addEventListener('keypress' , () =>{
+    if(senha1.value.length > 20)
+        senha1.style.border = "2px solid red";
+});
+
+[senha1, senha2].forEach(senha => {
+    senha.addEventListener('click', function() {
+        if (senha.style.backgroundImage = "url('/src/código-fonte/img/pass_Invisible.png')") {
+            senha.style.backgroundImage = "url('/src/código-fonte/img/pass_open.png')";
+            senha.setAttribute("type", "text");
+        } else {
+            senha.style.backgroundImage = "url('/src/código-fonte/img/pass_Invisible.png')";
+        }
+    });
 });
