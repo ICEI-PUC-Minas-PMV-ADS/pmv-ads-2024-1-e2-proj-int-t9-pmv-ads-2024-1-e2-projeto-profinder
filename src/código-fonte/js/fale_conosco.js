f57ddcbc4207dd4af6327d9eeccdd93e5e671368
mateus.salome@sga.pucminas.br <1494220@sga.pucminas.br>
2024-05-02 16:42:16 -0300

    Atualização js aba "fale conosco"

--- src/código-fonte/js/fale_conosco.js
+++ src/código-fonte/js/fale_conosco.js
@@ -23,5 +23,51 @@ document.getElementById('telefone').addEventListener('input', function (e) {
     e.target.value = unmaskedTelefone;
 });
 
+function validarEmail(email) {
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
 
+function onlyLetters(event) {
+    const regex = /[^A-Za-z\s]/g; 
+    let key = String.fromCharCode(event.charCode); 
+    if (regex.test(key)) {
+      event.preventDefault();
+    }
+  }
 
+document.getElementById('verif-form').addEventListener('submit', function(event) {
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var telefone = document.getElementById('telefone').value;
+    var mensagem = document.getElementById('mensagem').value;
+
+    if (!nome) {
+        event.preventDefault(); 
+        document.getElementById('errorMessage1').style.display = 'block';
+    }
+
+    if (!email) {
+        event.preventDefault(); 
+        document.getElementById('errorMessage2').style.display = 'block';
+    }
+
+    if (!validarEmail(email)) {
+        event.preventDefault();
+        document.getElementById('errorMessage2').style.display = 'block';
+    }
+
+    if (telefone.length !== 16) {
+        event.preventDefault();
+        document.getElementById('errorMessage3').style.display = 'block';
+    }
+
+    if (!mensagem) {
+        event.preventDefault(); 
+        document.getElementById('errorMessage4').style.display = 'block';
+    }
+
+    if (nome && email && validarEmail(email) && telefone.length === 16 && mensagem) {
+        alert('Mensagem enviada com sucesso!');
+    } 
+});
