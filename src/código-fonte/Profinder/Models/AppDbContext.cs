5a19a8599ab98285eb88f46ef847168714cf067c
Brian Mucio Duarte <106874869+brianmduarte@users.noreply.github.com>
2024-05-16 18:32:33 -0300

    .

--- src/código-fonte/Profinder/Models/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace Profinder.Models 
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
-    }
-}
