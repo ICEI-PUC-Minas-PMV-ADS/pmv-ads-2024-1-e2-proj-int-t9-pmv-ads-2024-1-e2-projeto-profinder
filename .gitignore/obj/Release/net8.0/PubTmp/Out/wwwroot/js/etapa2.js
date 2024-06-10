c8045e06c06509fba3bdd335d8072205f26c235c
Brian Mucio Duarte <braianmucioduarte@gmail.com>
2024-06-09 22:08:11 -0300

    Ajustes

---
+++ .gitignore/obj/Release/net8.0/PubTmp/Out/wwwroot/js/etapa2.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var statusElement = document.getElementById("status");
+    var btnAvancar = document.getElementById("btnAvancar");
+    var status = statusElement.getAttribute("data-status");
+
+    if (status === "Confirmado") {
+        btnAvancar.disabled = false;
+        btnAvancar.addEventListener("click", function () {
+            window.location.href = "/Contratacao/Etapa3"; // Ajuste o URL conforme necessário
+        });
+    } else {
+        btnAvancar.disabled = true;
+    }
+});
