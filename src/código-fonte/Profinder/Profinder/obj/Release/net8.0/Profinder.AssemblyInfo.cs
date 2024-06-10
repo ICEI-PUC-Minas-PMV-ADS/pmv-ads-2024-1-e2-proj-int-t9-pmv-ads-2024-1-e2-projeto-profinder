61e2e2dbf8de03b83070a9b8b8466b40d15c3b82
Brian Mucio Duarte <braianmucioduarte@gmail.com>
2024-06-09 21:56:47 -0300

    Deploy com últimas implementações

--- src/código-fonte/Profinder/Profinder/obj/Release/net8.0/Profinder.AssemblyInfo.cs
+++ src/código-fonte/Profinder/Profinder/obj/Release/net8.0/Profinder.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Profinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b17b2f7bc996fe8049ce1f697dec6f7a7009ab4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8359e386a0746d106692a5876cd6d5230d1d4a18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Profinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Profinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
